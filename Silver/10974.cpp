ed1439984fa0d09d738d33bda5c16d51897a5b39
jylee7665 <45624216+jylee7665@users.noreply.github.com>
2022-02-11 21:59:00 +0900

    Create 10974.cpp
    https://www.acmicpc.net/problem/10974

---
+++ Silver/10974.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	vector<int> v;
+	for (int i = 1; i <= n; i++) v.push_back(i);
+	sort(v.begin(), v.end());
+	do {
+		for (int i = 0; i < v.size(); i++) {
+			cout << v[i] << " ";
+		}
+		cout << "\n";
+	} while (next_permutation(v.begin(), v.end()));
+	
+}
